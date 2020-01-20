e5c9afe7ad03478803bc98095e8d035efd1289a3
Markus Schneider <cripple@gmx.de>
2020-01-20 22:12:49 +0100

    added ps script fpr node install

---
+++ setlist/init/install-node.ps1
@@ -0,0 +1,10 @@
+﻿# https://spin.atomicobject.com/2018/06/18/windows-node-js/
+
+#if permission is not set
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+
+#download and install 7zip and scoop
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+#download and install nodejs with nvm for version management
+scoop install nodejs nvm
